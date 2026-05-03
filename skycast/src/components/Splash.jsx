492f0d4da937daf1a9930a59b6b98ea3973928a4
asim770 <majiasim.1234@gmail.com>
2026-05-04 01:53:46 +0530

    k

---
+++ skycast/src/components/Splash.jsx
@@ -0,0 +1,14 @@
+import SplashCursor from './SplashCursor'
+
+<SplashCursor
+  DENSITY_DISSIPATION={3.5}
+  VELOCITY_DISSIPATION={2}
+  PRESSURE={0.1}
+  CURL={3}
+  SPLAT_RADIUS={0.2}
+  SPLAT_FORCE={6000}
+  COLOR_UPDATE_SPEED={10}
+  SHADING
+  RAINBOW_MODE={false}
+  COLOR="#A855F7"
+/>
